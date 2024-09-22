c801772cd8175b8d7a31f34ca3ed4ee35b74791d
Naeem <naeemnoman0419@gmail.com>
2024-09-23 01:53:37 +0600

    my project

---
+++ app/src/main/java/com/example/userprofileregistaion/UpdateProfileActivity.kt
@@ -0,0 +1,14 @@
+package com.example.userprofileregistaion
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class UpdateProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update_profile)
+    }
+}
